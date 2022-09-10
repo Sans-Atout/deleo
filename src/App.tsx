cb8db8b3672cf943a52b95230eac8066e590a886
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-09-10 12:49:26 +0200

    [HEADER] - Add basic header component
    [+] Add color variable in CSS
    
    [+] Add header component
    
    [+] Add "add trash" button GUI
    [+] Add "add folder" button GUI
    [+] Add "add file" button GUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import './main-component/base.css';
+import Header from './header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header hasButon={true}></Header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.css';
+import {AddTrashBtn, AddFolderBtn, AddFileBtn} from '../btn/Button';
+import logo from './logo.svg';
+
+class Header extends React.Component<{hasButon:boolean}>{
+
+
+
+    render() {
+        return <header>
+            <div className='logo-zone'>
+                <img className='logo-img' src={logo} alt='deleo logo'/>
+                <div className='app-info-zone'>
+                    <p className='app-name'>deleo</p>
+                    <p className='app-version'>v1.0.0 (64bits)</p>
+                </div>
+            </div>
+            <div className='btn-zone'>
+                <AddFolderBtn/>
+                <AddFileBtn/>
+                <AddTrashBtn/>
+
+            </div>
+        </header>
+    }
+}
+
+export default Header;
