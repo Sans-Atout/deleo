771f7ca8509cce07832950a59f7136a85e2a8b76
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-09-12 23:18:31 +0200

    [CONFIG] - Trying config logic
    [+] Adding basic dimension in tauri conf file
    
    [+] Get user config folder
    [+] Writing a config file 
    [+] Reading a config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,87 @@
 import React from 'react';
 import './main-component/base.css';
 import Header from './header/Header';
-import { EraseContent, SettingContent, TabLabel, WikiContent } from './tabs/Tabs'; 
-import {EraseTask, ErasingMethod, Status, } from './row/ErasingRow';
+import { EraseContent, SettingContent, TabLabel, WikiContent } from './tabs/Tabs';
+import { EraseTask, ErasingMethod, Status, } from './row/ErasingRow';
 import uuid from 'react-uuid';
+import { configDir } from '@tauri-apps/api/path';
 
 import './App.css';
 
 //Icons 
 import settings from './images/settings.svg';
 import book from './images/book.svg';
-import trash from './images/trash.svg'; 
+import trash from './images/trash.svg';
+import { fs } from '@tauri-apps/api';
 
-class App extends React.Component {
+class App extends React.Component<{}, { app_path: string, configuration: {} }> {
 
-  //allEraseObject : EraseRow[];
+  readConfigFile() {
+    (async () => {
+      var deleo_config: {};
+      if (! await this.doesConfigExist()) {
+        deleo_config = { langage: "en", delete_task: {} }
+        await fs.writeFile(this.state.app_path + "deleo.json", JSON.stringify(deleo_config));
+      }
+      let file_content = fs.readTextFile(this.state.app_path + "deleo.json")
+      console.log(file_content)
+
+    })().catch(error => {
+      console.log(error)
+    });
+
+  }
+
+  async doesConfigExist() {
+    return await fs.readTextFile(this.state.app_path + 'deleo.json').then(
+      () => {
+        return true;
+      }
+    ).catch(
+      (error) => {
+        return false;
+      }
+    )
+  }
+
+  componentDidMount() {
+
+    (async () => {
+      const config_path = await configDir();
+      this.setState({ app_path: config_path },this.readConfigFile)
+
+    })()
+      .catch(error => {
+        console.log(error)
+      });
+
+  }
+
 
   render(): React.ReactNode {
+    var hello = [<EraseTask key={uuid()} name='My First Erasing Task' method={ErasingMethod.RCMP} status={Status.ToDo} paths={["trash:///container"]} />, <EraseTask key={uuid()} name='My First Erasing Task' method={ErasingMethod.RCMP} status={Status.ToDo} paths={["trash:///container"]} />]
     return <div className="App">
-        <Header hasButon={true}></Header>
-          <div className='container'>
-            <div className='tabs-list'>
-              <TabLabel icons={trash} label="Erase"/>
-              <TabLabel icons={book} label='Doc'/>
-              <TabLabel icons={settings} label='Settings'/>
-            </div>
-            <div className='main-info'>
-              <EraseContent isVisible={true}>
-                <EraseTask key={uuid()} name='My First Erasing Task' method={ErasingMethod.RCMP} status={Status.ToDo} paths={["trash:///container"]}/>
-  
-              </EraseContent>
-              <SettingContent isVisible={false}>
-  
-              </SettingContent>
-              <WikiContent isVisible={false}/>
-              
-            </div>
-          </div>
-      </div>;
-  
+      <Header hasButon={true}></Header>
+      <div className='container'>
+        <div className='tabs-list'>
+          <TabLabel icons={trash} label="Erase" />
+          <TabLabel icons={book} label='Doc' />
+          <TabLabel icons={settings} label='Settings' />
+        </div>
+        <div className='main-info'>
+          <EraseContent isVisible={true}>
+            {hello}
+
+          </EraseContent>
+          <SettingContent isVisible={false}>
+
+          </SettingContent>
+          <WikiContent isVisible={false} />
+
+        </div>
+      </div>
+    </div>;
+
   }
 }
 
